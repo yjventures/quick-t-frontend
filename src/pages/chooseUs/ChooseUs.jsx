b2745be79db5e6e717fa2749a7cf38d6fc54a276
ahadalichowdhury <smahadalichowdhury@gmail.com>
2023-11-18 10:34:07 +0600

    modified our services page

--- src/pages/chooseUs/ChooseUs.jsx
+++ src/pages/chooseUs/ChooseUs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import color from "../../assets/images/color.png";
 import "./chooseUs.css";
 function ChooseUs({
   service_box_one_icon,
@@ -17,403 +18,80 @@ function ChooseUs({
         <p className="chooseUsTextIntro ">WE HELP FOR YOU</p>
         <p className="chooseUsTextIntroHeading text-4xl md:text-5xl lg:text-5xl xl:text-5xl pt-5">
           {/* look here image is showing,  */}
-          Why Chooes Us <img src={`http://localhost:1337${service_box_one_icon}`} alt="icon" />
+          Why Chooes Us{" "}
+          <img
+            src={`http://localhost:1337${service_box_one_icon}`}
+            alt="icon"
+          />
         </p>
       </div>
       <div className="container mx-auto">
-        <div className="flex flex-wrap justify-center lg:justify-between items-center">
-          <div className="w-full sm:w-2/2 md:w-auto lg:w-auto p-4">
-            <svg
-              width="334"
-              height="346"
-              viewBox="0 0 434 346"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect
-                x="91.3691"
-                y="50.5835"
-                width="327.616"
-                height="281.919"
-                rx="6.69167"
-                transform="rotate(-3 91.3691 50.5835)"
-                fill="#EDF0F6"
-              />
-              <g filter="url(#filter0_d_305_9069)">
-                <rect
-                  x="27.7166"
-                  y="57.2751"
-                  width="375.849"
-                  height="247.592"
-                  rx="6.69167"
-                  fill="white"
-                />
-              </g>
-              <svg width="200" height="100" xmlns="http://www.w3.org/2000/svg">
-                <rect
-                  x="66.7507"
-                  y="0.394775"
-                  width="99.5385"
-                  height="93.6833"
-                  rx="6.69167"
-                  fill="#40CC9C"
-                />
-                {
-                  service_box_one_icon ? <img src={`http://localhost:1337${service_box_one_icon}`} alt="icon" /> :
-                    <foreignObject x="93" y="18" width="50" height="49">
-                      <svg
-                        width="50"
-                        height="49"
-                        viewBox="0 0 50 49"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M17.7587 24.7853L22.8394 29.8324L33.0009 19.7382M47.2726 9.60369C39.2402 10.0274 31.3667 7.26843 25.3798 1.93213C19.3929 7.26843 11.5194 10.0274 3.48703 9.60369C2.84059 12.0897 2.51451 14.6471 2.51661 17.2147C2.51661 31.3238 12.2309 43.1819 25.3798 46.5432C38.5287 43.1819 48.243 31.3263 48.243 17.2147C48.243 14.5851 47.9051 12.0364 47.2726 9.60369Z"
-                          stroke="white"
-                          stroke-width="3.34583"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                      </svg>
-                    </foreignObject>
-                }
-              </svg>
-
-              <text
-                x="50"
-                y="150"
-                fill="#07225A"
-                fontSize={24}
-                fontWeight={600}
-              >
-                {
-                  service_box_one_title ? service_box_one_title : "Trust Partner"
-                }
-              </text>
-
-              <text
-                x="50"
-                y="200"
-                fill="#07225A"
-                fontSize={19}
-                fontWeight={400}
-              >
-                {
-                  service_box_one_desc ? service_box_one_desc : "Cloudless and of a deep dark blue the spectacle before us was."
-                }
-              </text>
-
-              <defs>
-                <filter
-                  id="filter0_d_305_9069"
-                  x="0.949886"
-                  y="43.8918"
-                  width="429.382"
-                  height="301.125"
-                  filterUnits="userSpaceOnUse"
-                  color-interpolation-filters="sRGB"
-                >
-                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                  <feColorMatrix
-                    in="SourceAlpha"
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                    result="hardAlpha"
-                  />
-                  <feOffset dy="13.3833" />
-                  <feGaussianBlur stdDeviation="13.3833" />
-                  <feColorMatrix
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.0751529 0"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in2="BackgroundImageFix"
-                    result="effect1_dropShadow_305_9069"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="effect1_dropShadow_305_9069"
-                    result="shape"
-                  />
-                </filter>
-              </defs>
-            </svg>
+        <div className="flex flex-wrap justify-center lg:justify-between items-center mt-20">
+          <div className="w-full sm:w-2/2 md:w-auto lg:w-auto p-4 mt-20">
+            <div style={{ position: "relative" }}>
+              <div className="services_bgCard" style={{}}></div>
+              <div className="services_card">
+                <img src={color} alt="icon" className="card_topImage" />
+                <div className="card_content">
+                  <div className="card_content_title">
+                    <p>
+                      {service_box_three_title
+                        ? service_box_three_title
+                        : "24/7 Support"}
+                    </p>
+                  </div>
+                  <div className="card_content_desc">
+                    {service_box_three_desc
+                      ? service_box_three_desc
+                      : "Cloudless and of a deep dark blue the spectacle before us was."}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="w-full sm:w-2/2 md:w-auto lg:w-auto p-4">
-            <svg
-              width="333"
-              height="346"
-              viewBox="0 0 433 346"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect
-                x="90.6721"
-                y="50.5835"
-                width="327.616"
-                height="281.919"
-                rx="6.69167"
-                transform="rotate(-3 90.6721 50.5835)"
-                fill="#EDF0F6"
-              />
-              <g filter="url(#filter0_d_305_9077)">
-                <rect
-                  x="27.0198"
-                  y="57.2751"
-                  width="375.849"
-                  height="247.592"
-                  rx="6.69167"
-                  fill="white"
-                />
-              </g>
-              <svg
-                width="165.5385"
-                height="93.6833"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  x="66.0549"
-                  y="0.394775"
-                  width="99.5385"
-                  height="93.6833"
-                  rx="6.69167"
-                  fill="#FFB88A"
-                />
-                {
-                  service_box_two_icon ? <img src={`http://localhost:1337${service_box_two_icon}`} alt="icon" /> :
-                    <svg
-                      x="95.0549"
-                      y="23.394775"
-                      width="49"
-                      height="49"
-                      viewBox="0 0 49 49"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M38.672 22.6831H10.6971C8.48994 22.6831 6.70068 24.4724 6.70068 26.6795V40.667C6.70068 42.8741 8.48994 44.6634 10.6971 44.6634H38.672C40.8791 44.6634 42.6684 42.8741 42.6684 40.667V26.6795C42.6684 24.4724 40.8791 22.6831 38.672 22.6831Z"
-                        stroke="white"
-                        stroke-width="3.34583"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                      <path
-                        d="M14.6938 22.6831V14.6902C14.6938 12.0405 15.7465 9.49921 17.6202 7.62552C19.4938 5.75184 22.0351 4.69922 24.6849 4.69922C27.3347 4.69922 29.8759 5.75184 31.7496 7.62552C33.6233 9.49921 34.6759 12.0405 34.6759 14.6902V22.6831"
-                        stroke="white"
-                        stroke-width="3.34583"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      />
-                    </svg>
-                }
-              </svg>
-
-              <text
-                x="50"
-                y="150"
-                fill="#07225A"
-                fontSize={24}
-                fontWeight={600}
-              >
-                {service_box_two_title ? service_box_two_title : "Secure Processing"}
-              </text>
-
-              <text
-                x="50"
-                y="200"
-                fill="#07225A"
-                fontSize={19}
-                fontWeight={400}
-              >
-                {
-                  service_box_two_desc ? service_box_two_desc : "Cloudless and of a deep dark blue the spectacle before us was."
-                }
-              </text>
-
-              <defs>
-                <filter
-                  id="filter0_d_305_9077"
-                  x="0.253109"
-                  y="43.8918"
-                  width="429.382"
-                  height="301.125"
-                  filterUnits="userSpaceOnUse"
-                  color-interpolation-filters="sRGB"
-                >
-                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                  <feColorMatrix
-                    in="SourceAlpha"
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                    result="hardAlpha"
-                  />
-                  <feOffset dy="13.3833" />
-                  <feGaussianBlur stdDeviation="13.3833" />
-                  <feColorMatrix
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.0751529 0"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in2="BackgroundImageFix"
-                    result="effect1_dropShadow_305_9077"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="effect1_dropShadow_305_9077"
-                    result="shape"
-                  />
-                </filter>
-              </defs>
-            </svg>
+          <div className="w-full sm:w-2/2 md:w-auto lg:w-auto p-4 mt-20">
+            <div style={{ position: "relative" }}>
+              <div className="services_bgCard" style={{}}></div>
+              <div className="services_card">
+                <img src={color} alt="icon" className="card_topImage" />
+                <div className="card_content">
+                  <div className="card_content_title">
+                    <p>
+                      {service_box_three_title
+                        ? service_box_three_title
+                        : "24/7 Support"}
+                    </p>
+                  </div>
+                  <div className="card_content_desc">
+                    {service_box_three_desc
+                      ? service_box_three_desc
+                      : "Cloudless and of a deep dark blue the spectacle before us was."}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="w-full sm:w-2/2 md:w-auto lg:w-auto p-4">
-            <svg
-              width="333"
-              height="346"
-              viewBox="0 0 433 346"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect
-                x="90.6721"
-                y="50.5835"
-                width="327.616"
-                height="281.919"
-                rx="6.69167"
-                transform="rotate(-3 90.6721 50.5835)"
-                fill="#EDF0F6"
-              />
-              <g filter="url(#filter0_d_305_9077)">
-                <rect
-                  x="27.0198"
-                  y="57.2751"
-                  width="375.849"
-                  height="247.592"
-                  rx="6.69167"
-                  fill="white"
-                />
-              </g>
-              <svg
-                width="166"
-                height="93.6833"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  x="66.0549"
-                  y="0.394775"
-                  width="99.5385"
-                  height="93.6833"
-                  rx="6.69167"
-                  fill="#04BEFE"
-                />
-                {
-                  service_box_three_icon ? <img src={`http://localhost:1337${service_box_three_icon}`} alt="icon" /> :
-                    <svg
-                      x="91.8249"
-                      y="23.34115"
-                      width="49"
-                      height="49"
-                      viewBox="0 0 49 49"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <g clip-path="url(#clip0_305_9102)">
-                        <path
-                          d="M24.1032 30.6743C31.8282 30.6743 38.0906 24.412 38.0906 16.6869C38.0906 8.96185 31.8282 2.69946 24.1032 2.69946C16.3781 2.69946 10.1157 8.96185 10.1157 16.6869C10.1157 24.412 16.3781 30.6743 24.1032 30.6743Z"
-                          stroke="white"
-                          stroke-width="3.34583"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                        <path
-                          d="M16.5306 28.458L14.1128 46.6616L24.1038 40.667L34.0949 46.6616L31.677 28.438"
-                          stroke="white"
-                          stroke-width="3.34583"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                      </g>
-                      <defs>
-                        <clipPath id="clip0_305_9102">
-                          <rect
-                            width="47.9569"
-                            height="47.9569"
-                            fill="white"
-                            transform="translate(0.125 0.70166)"
-                          />
-                        </clipPath>
-                      </defs>
-                    </svg>
-
-                }
-              </svg>
-
-              <text
-                x="50"
-                y="150"
-                fill="#07225A"
-                fontSize={24}
-                fontWeight={600}
-              >
-                {
-                  service_box_three_title ? service_box_three_title : "Award-Winning Team"
-                }
-              </text>
-
-              <text
-                x="50"
-                y="200"
-                fill="#07225A"
-                fontSize={19}
-                fontWeight={400}
-              >
-                {
-                  service_box_three_desc ? service_box_three_desc : "Cloudless and of a deep dark blue the spectacle before us was."
-                }
-              </text>
-
-              <defs>
-                <filter
-                  id="filter0_d_305_9077"
-                  x="0.253109"
-                  y="43.8918"
-                  width="429.382"
-                  height="301.125"
-                  filterUnits="userSpaceOnUse"
-                  color-interpolation-filters="sRGB"
-                >
-                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                  <feColorMatrix
-                    in="SourceAlpha"
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                    result="hardAlpha"
-                  />
-                  <feOffset dy="13.3833" />
-                  <feGaussianBlur stdDeviation="13.3833" />
-                  <feColorMatrix
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.0751529 0"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in2="BackgroundImageFix"
-                    result="effect1_dropShadow_305_9077"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="effect1_dropShadow_305_9077"
-                    result="shape"
-                  />
-                </filter>
-              </defs>
-            </svg>
+          <div className="w-full sm:w-2/2 md:w-auto lg:w-auto p-4 mt-20">
+            <div style={{ position: "relative" }}>
+              <div className="services_bgCard" style={{}}></div>
+              <div className="services_card">
+                <img src={color} alt="icon" className="card_topImage" />
+                <div className="card_content">
+                  <div className="card_content_title">
+                    <p>
+                      {service_box_three_title
+                        ? service_box_three_title
+                        : "24/7 Support"}
+                    </p>
+                  </div>
+                  <div className="card_content_desc">
+                    {service_box_three_desc
+                      ? service_box_three_desc
+                      : "Cloudless and of a deep dark blue the spectacle before us was."}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
