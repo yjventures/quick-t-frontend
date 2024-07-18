e7edd92b4385614148f86af0330cae0a1355f21f
Mahinur Rahman <mahi121.mr2@gmail.com>
2024-07-18 16:08:27 +0600

    time removed from order id

--- src/pages/TransferOTP/TransferOTP.jsx
+++ src/pages/TransferOTP/TransferOTP.jsx
@@ -77,7 +77,7 @@ function TransferOTP() {
       // transaction_id of db is order id here
       // console.log(res?.data)
       const time = Math.floor(Date.now() / 1000);
-      const order_id = res?.data?.order_id + '_' + time;
+      const order_id = res?.data?.order_id;
       // order_id.toString();
       const amountData = localStorage.getItem("amountData");
       localStorage.setItem("order_id", order_id);
