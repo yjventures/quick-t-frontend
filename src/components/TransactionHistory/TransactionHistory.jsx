87c93274a8a4404da70e136801aed9c8f210c2e7
Mahinur Rahman <mahi121.mr2@gmail.com>
2024-08-14 15:17:02 +0600

    all dashboard pages completed

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -8,8 +8,8 @@ function TransactionHistory() {
   useEffect(() => {
     let userId = localStorage.getItem("user_id");
     axios.get(`https://api.quickt.com.au/api/transactions?populate=*&filters[users_permissions_user][id][$eq]=${userId}`, {
-      headers : {
-        Authorization : `Bearer ${localStorage.getItem("jwt")}`
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`
       }
     })
       .then((res) => {
@@ -17,7 +17,7 @@ function TransactionHistory() {
           (a, b) =>
             b.attributes.transaction_date - a.attributes.transaction_date
         );
-        console.log("transaction data is", sortedTransactions);
+        // console.log("transaction data is", sortedTransactions);
         setTransactionData(sortedTransactions);
       })
       .catch((err) => {
@@ -26,17 +26,12 @@ function TransactionHistory() {
   }, []);
 
   function formatTimestampToDDMMYYYY(timestamp) {
-    const date = new Date(timestamp * 1000);
-
-    const day = date.getDate();
-    const month = date.getMonth() + 1; // Months are zero-based
-    const year = date.getFullYear();
-
-    const formattedDate = `${day < 10 ? "0" : ""}${day}-${
-      month < 10 ? "0" : ""
-    }${month}-${year}`;
-
-    return formattedDate;
+    const transaction_date = new Date(timestamp);
+    return transaction_date.toLocaleDateString('us-EN', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    });
   }
 
   //////////////////////////
@@ -55,30 +50,28 @@ function TransactionHistory() {
     }
   }
   return (
-    <div style={{ backgroundColor: "#f5f5f5", paddingTop: "50px" }}>
+    <div className="min-h-screen" style={{ backgroundColor: "#f5f5f5", paddingTop: "50px" }}>
       <div
-        className="card"
+        className="card min-h-[90vh]"
         style={{ maxWidth: "1200px", backgroundColor: "#fff" }}
       >
-        <div>
-          <NavLink to="/" className="flex items-center gap-3">
-            <div>
-              <svg
-                width="18"
-                height="13"
-                viewBox="0 0 18 13"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M18 5.5H3.83L7.41 1.91L6 0.5L0 6.5L6 12.5L7.41 11.08L3.83 7.5H18V5.5Z"
-                  fill="#333333"
-                />
-              </svg>
+        <p
+          className="mb-10"
+          style={{
+            color: "#2F80ED",
+            fontSize: "20px",
+            fontWeight: "500",
+          }}
+        >
+          Transaction History
+        </p>
+        {
+          transactionData.length === 0 && (
+            <div className="flex justify-center items-center h-[50vh]">
+              <p className="text-lg text-gray-500">No transaction found</p>
             </div>
-            <p style={{ fontSize: "15px" }}>Back to home</p>
-          </NavLink>
-        </div>
+          )
+        }
         {transactionData.map((transaction, index) => {
           return (
             <div key={index}>
@@ -92,9 +85,9 @@ function TransactionHistory() {
                 </div>
                 <div className="space-y-2">
                   <div className="grid sm:grid-cols-1 md:grid-cols-5 lg:grid-cols-5 gap-8 items-center">
-                    <div className="text-center">
+                    <div className="text-start pt-4">
                       <p className="text-sm text-slate-700">Receiver Name</p>
-                      <p className="text-md">
+                      <p className="text-md mt-1">
                         {transaction.attributes.receiver_name
                           .split(" ")
                           .slice(0, 4)
@@ -107,29 +100,27 @@ function TransactionHistory() {
                     </div>
                     <div className="text-center">
                       <p className="text-sm text-slate-700">Receiver Number</p>
-                      <p className="text-md">
+                      <p className="text-md mt-1 ">
                         {transaction.attributes.receiver_number}
                       </p>
                     </div>
                     <div className="text-center">
-                      <p className="text-sm text-slate-700">Status</p>
+                      <p className="text-sm text-slate-700">Gateway Status</p>
                       <p
-                        className={`text-md ${getStatusColorClass(
-                          transaction.attributes.payment_status
-                        )}`}
+                        className={`text-md bg-gray-500 text-white rounded-full mt-1`}
                       >
-                        {transaction?.attributes?.payment_status}
+                        {transaction?.attributes?.stripe_status}
                       </p>
                     </div>
                     <div className="text-center">
-                      <p className="text-sm text-slate-700">Amount</p>
-                      <p className="text-md">
-                        {transaction.attributes.amount_total +
+                      <p className="text-sm text-slate-700">Amount </p>
+                      <p className="text-md mt-1">
+                        {transaction.attributes.transfer_amount +
                           " " +
                           transaction.attributes.currency}
                       </p>
                     </div>
-                    <div className="col-span-2 md:col-span-1 lg:col-span-1 text-center">
+                    <div className="col-span-2 md:col-span-1 lg:col-span-1 text-center mt-2">
                       <button
                         className="bg-white border border-gray-500 text-black py-2 px-4 rounded-md"
                         onClick={() => setShowModalIndex(index)}
@@ -140,7 +131,7 @@ function TransactionHistory() {
                     {showModalIndex === index && (
                       <>
                         <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                          <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                          <div className="relative w-auto my-6 mx-auto max-w-[600px]">
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                               {/*header*/}
@@ -161,7 +152,7 @@ function TransactionHistory() {
                               <div className="relative p-6 flex-auto">
                                 <div className="flex flex-col md:flex-row lg:flex-row justify-between gap-4 md:gap-20 lg:gap-32">
                                   {/* for left side */}
-                                  <div>
+                                  <div className="flex justify-center items-start flex-col">
                                     <div>
                                       <p className="text-sm text-slate-700 ">
                                         Receiver Name
@@ -189,7 +180,7 @@ function TransactionHistory() {
                                         }
                                       </p>
                                     </div>
-                                    <div>
+                                    {/* <div>
                                       <p className="text-sm text-slate-700 mt-4">
                                         Payment Status
                                       </p>
@@ -200,7 +191,7 @@ function TransactionHistory() {
                                       >
                                         {transaction.attributes.payment_status}
                                       </p>
-                                    </div>
+                                    </div> */}
                                     <div>
                                       <p className="text-sm text-slate-700 mt-4">
                                         Transfer Perpose
@@ -214,31 +205,90 @@ function TransactionHistory() {
                                     </div>
                                   </div>
                                   {/* for right side */}
-                                  <div>
+                                  <div className="flex flex-col gap-3">
+                                    {/* paid total amount */}
                                     <div>
                                       <p className="text-sm text-slate-700">
-                                        Amount
+                                        Sender Paid
                                       </p>
                                       <p className="text-md">
-                                        {transaction.attributes.amount_total}
+                                        {transaction.attributes.amount_total} {transaction.attributes.currency}
                                       </p>
                                     </div>
+
+                                    {/* Fees deducted */}
+                                    <div>
+                                      <p className="text-sm text-slate-700">
+                                        Fees
+                                      </p>
+                                      <p className="text-md">
+                                        {transaction.attributes.amount_total - transaction.attributes.transfer_amount} {transaction.attributes.currency}
+                                      </p>
+                                    </div>
+
+                                    {/* Receivers amount */}
+                                    <div>
+                                      <p className="text-sm text-slate-700">
+                                        Receiver Receives
+                                      </p>
+                                      <p className="text-md">
+                                        {transaction.attributes.transfer_amount} {transaction.attributes.currency}
+                                      </p>
+                                    </div>
+
                                     <div>
                                       <p className="text-sm text-slate-700 mt-4">
-                                        Payment Method
+                                        Payment Date
                                       </p>
                                       <p className="text-md">
                                         {
-                                          transaction.attributes
-                                            .payment_method_type
+                                          formatTimestampToDDMMYYYY(
+                                            transaction.attributes.transaction_date
+                                          )
                                         }
                                       </p>
                                     </div>
                                   </div>
                                 </div>
+                                {/* note if available */}
+                                {
+                                  transaction?.attributes?.note && (
+                                    <div className="mt-2">
+                                      <p className="text-sm text-slate-700">
+                                        Note
+                                      </p>
+                                      <p className="text-md">
+                                        {transaction?.attributes?.note}
+
+                                      </p>
+                                    </div>)
+                                }
                               </div>
                               {/*footer*/}
-                              <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                              <div className="flex items-center justify-between p-6 border-t border-solid border-blueGray-200 rounded-b">
+                                <div>
+                                  <div className="md:flex gap-2 items-center">
+                                    <p className="text-sm text-slate-700">
+                                      Password
+                                    </p>
+                                    <p className="text-md">
+                                      {transaction?.attributes?.transaction_password}
+                                    </p>
+                                    <p
+                                      className="w-5 h-5 cursor-pointer"
+                                      onClick={() => {
+                                        navigator.clipboard.writeText(transaction?.attributes?.transaction_password);
+                                        alert("Password copied to clipboard");
+                                      }}
+                                    >
+                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+                                      </svg>
+                                    </p>
+                                  </div>
+                                  <sub className=" font-bold">Never share your password*</sub>
+
+                                </div>
                                 <button
                                   className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                   type="button"
